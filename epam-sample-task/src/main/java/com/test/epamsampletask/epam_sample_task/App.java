86c6fda49196a1858ebfc8b9ee11fec655b334d3
TharunKaushal <tharunkaushal@gmail.com>
2019-03-11 10:21:13 +0530

    Sample Java Project

---
+++ epam-sample-task/src/main/java/com/test/epamsampletask/epam_sample_task/App.java
@@ -0,0 +1,13 @@
+package com.test.epamsampletask.epam_sample_task;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello EPAM!" );
+    }
+}
